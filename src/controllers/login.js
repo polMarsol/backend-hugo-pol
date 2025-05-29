9c43748bcd2a7708b3f1b3247f944cc20379b4f9
PolMarsolUdl <144933485+polMarsol@users.noreply.github.com>
2025-05-29 19:49:33 +0200

    Update login.js

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -27,7 +27,7 @@ loginRouter.post('/', async (request, response) => {
     };
 
 
-    const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: '1m' });
+    const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: '1h' });
     response
         .status(200)
         .send({ token, id: user.id, username: user.username, name: user.name, role: user.role });
